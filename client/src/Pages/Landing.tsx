b906ba9ca174cc53796da0905a57db1eea6bd496
Tommymarvel <ibironketomiwa4@gamil.com>
2024-05-12 15:51:24 +0100

    "Update Landing page components, add PartnersPerks section, update font imports and configurations in index.css and tailwind.config.js"

--- client/src/Pages/Landing.tsx
+++ client/src/Pages/Landing.tsx
@@ -1,11 +1,12 @@
 import {Navbar} from "../components"
-import { Authentic } from "../components/LandingPageComponents"
+import { Authentic, PartnersPerks } from "../components/LandingPageComponents"
 
 const Landing = () => {
   return (
     <div className="bg-black px-3 md:px-10 lg:px-14">
       <Navbar/>
       <Authentic/>
+      <PartnersPerks />
     </div>
   )
 }
--- client/src/components/LandingPageComponents/index.tsx
+++ client/src/components/LandingPageComponents/index.tsx
@@ -1,1 +1,3 @@
 export { default as Authentic } from "./Authentic";
+export { default as PartnersPerks } from "./Partners_Perks";
+
