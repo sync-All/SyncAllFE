09cefb0e16bd05343ba688f1a4b1402b74366e1e
Tommymarvel <ibironketomiwa4@gmail.com>
2024-06-21 19:36:37 +0100

    feat: Add profile setup API and update profile update route

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,59 +1,105 @@
 var express = require('express');
 const asynchandler = require('express-async-handler');
-const authcontroller = require('../controllers/authControllers')
-const passport = require('passport')
-const User = require("../models/usermodel")
+const authcontroller = require('../controllers/authControllers');
+const passport = require('passport');
+const User = require('../models/usermodel');
 
 var router = express.Router();
 
 /* GET users listing. */
 router.post('/api/v1/signup', asynchandler(authcontroller.signup));
 
-router.post('/api/v1/signin',asynchandler(authcontroller.signin))
+router.post('/api/v1/signin', asynchandler(authcontroller.signin));
 
-router.get('/api/v1/allusers', asynchandler(authcontroller.allUsers))
+router.get('/api/v1/allusers', asynchandler(authcontroller.allUsers));
 
-router.post('/api/v1/profilesetup/:userId', async (req,res,next)=>{
+router.post('/api/v1/profilesetup/:userId', async (req, res, next) => {
+  if (req.isAuthenticated) {
+    const { fullName, spotifyLink, bio } = req.body;
+    if (!fullName || !spotifyLink || !bio) {
+      res
+        .status(401)
+        .json({
+          success: false,
+          message: 'Missing field please check and confirm',
+        });
+    } else {
+      const userId = req.params.userId;
+      const profileUpdate = await User.findByIdAndUpdate(
+        userId,
+        { fullName, spotifyLink, bio },
+        { new: true }
+      );
 
-    if(req.isAuthenticated){
-        const {fullName, spotifyLink, bio} = req.body
-        if(!fullName || !spotifyLink || !bio){
-            res.status(401).json({success : false, message : 'Missing field please check and confirm'})
-        }else{
-            const userId = req.params.userId
-            const profileUpdate = await User.findByIdAndUpdate(userId, {fullName, spotifyLink, bio}, {new : true})
-
-            res.status(200).json({success : true, message : 'Profile update successful', profileUpdate})
-        }
-    }else{
-        res.status(401).json({success : false, message : "Unauthorized, Please proceed to login"})
+      res
+        .status(200)
+        .json({
+          success: true,
+          message: 'Profile update successful',
+          profileUpdate,
+        });
     }
-  })
+  } else {
+    res
+      .status(401)
+      .json({
+        success: false,
+        message: 'Unauthorized, Please proceed to login',
+      });
+  }
+});
 
-router.get('/verifyEmail/', passport.authenticate('jwt',{session : false, failureRedirect : '/notConfirmed'}), async (req,res,next)=>{
-    if(req.user.emailConfirmedStatus){
-        res.redirect('/AlreadyConfirmed')
+router.get(
+  '/verifyEmail/',
+  passport.authenticate('jwt', {
+    session: false,
+    failureRedirect: '/notConfirmed',
+  }),
+  async (req, res, next) => {
+    if (req.user.emailConfirmedStatus) {
+      res.redirect('/AlreadyConfirmed');
+    } else if (req.isAuthenticated()) {
+      const user = await User.findOneAndUpdate(
+        { _id: req.user._id },
+        { emailConfirmedStatus: true },
+        { new: true }
+      );
+      res.redirect('/confirmedEmail');
+    } else {
+      res.redirect('/notConfirmed');
     }
-    else if(req.isAuthenticated()){
-        const user = await User.findOneAndUpdate({_id : req.user._id},{emailConfirmedStatus : true},{new : true})
-        res.redirect('/confirmedEmail')
-    }else{
-        res.redirect('/notConfirmed')
-    }
-})
+  }
+);
 
 // , {session : false, successRedirect : '/confirmedEmail', failureRedirect : '/notConfirmed'}
 
-router.get('/confirmedEmail',(req,res,next)=>{
-    res.render('emailConfirmed', {title : 'Email Verified',icon : '/images/verifiedIcon.png',heading : "Email Verified", message : "Your email address has been successfully verified. Click below to proceed."})
-})
+router.get('/confirmedEmail', (req, res, next) => {
+  res.render('emailConfirmed', {
+    title: 'Email Verified',
+    icon: '/images/verifiedIcon.png',
+    heading: 'Email Verified',
+    message:
+      'Your email address has been successfully verified. Click below to proceed.',
+  });
+});
 
-router.get('/notConfirmed',(req,res,next)=>{
-    res.render('emailConfirmed', {title : 'Not Verified', icon : '/images/notverified.png',heading : "Email Not Verified", message : "Looks like the link has expired, kindly proceed to Login to get another link sent"})
-})
+router.get('/notConfirmed', (req, res, next) => {
+  res.render('emailConfirmed', {
+    title: 'Not Verified',
+    icon: '/images/notverified.png',
+    heading: 'Email Not Verified',
+    message:
+      'Looks like the link has expired, kindly proceed to Login to get another link sent',
+  });
+});
 
-router.get('/AlreadyConfirmed',(req,res,next)=>{
-    res.render('emailConfirmed', {title : 'Already Confirmed', icon : '/images/verifiedIcon.png',heading : "Email Already Been Setup", message : "Please proceed to login below"})
-})
+router.get('/AlreadyConfirmed', (req, res, next) => {
+  res.render('emailConfirmed', {
+    title: 'Already Confirmed',
+    icon: '/images/verifiedIcon.png',
+    heading: 'Email Already Been Setup',
+    message: 'Please proceed to login below',
+  });
+});
 
 module.exports = router;
