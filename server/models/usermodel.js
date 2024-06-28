23e2f2fd3706681471318657f08a36ce9e15de79
oladee <deemajor230600@gmail.com>
2024-06-28 13:15:59 +0100

    included phone Number in schema

--- server/models/usermodel.js
+++ server/models/usermodel.js
@@ -33,6 +33,9 @@ const userSchema = new Schema({
         enum : ['local', 'googleAuth'],
         default : 'local'
     },
+    phoneNumber :{
+        type : String
+    },
     fullName : {
         type : String,
     },
