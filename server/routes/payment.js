25ed82662dc1a775cded1341cf226af8e0d434ae
oladee <deemajor230600@gmail.com>
2024-09-04 21:37:09 +0100

    pushy

--- server/routes/payment.js
+++ server/routes/payment.js
@@ -5,10 +5,11 @@ const stripeApi = require('../utils/stripeApi')
 require('dotenv').config()
 const stripe = require('stripe')(process.env.STRIPE_TESTSECRET_KEY)
 
-router.post('/create-subcription',passport.authenticate('jwt',{session : false, failureRedirect : '/unauthorized'}),asyncHandler(async(req,res,next)=>{
+router.post('/create-subscription',passport.authenticate('jwt',{session : false, failureRedirect : '/unauthorized'}),asyncHandler(async(req,res,next)=>{
     const {priceId} = req.body
     const userInfo = req.user
-    const customerId = await stripeApi.createNewStripeCus(userInfo)
+    try {
+        const customerId = await stripeApi.createNewStripeCus(userInfo)
         const subscription = await stripe.subscriptions.create({
             customer : customerId,
             items : [
@@ -20,6 +21,9 @@ router.post('/create-subcription',passport.authenticate('jwt',{session : false, 
             payment_settings: { save_default_payment_method: 'on_subscription' },
             expand: ['latest_invoice.payment_intent'],
         })
+    } catch (error) {
+        console.log(error)
+    }
         res.send({priceId, customerId, subscription})
 }))
 
