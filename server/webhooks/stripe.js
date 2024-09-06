6d98fb2a89c80e9d4f0594aee022ef8dd2582f53
oladee <deemajor230600@gmail.com>
2024-09-06 09:11:40 +0100

    changed logic of webhook

--- server/webhooks/stripe.js
+++ server/webhooks/stripe.js
@@ -6,30 +6,26 @@ router.post('/stripe/webhook',async (req,res,next)=>{
         case 'customer.subscription.created':
             const subscription_created = event.data.object
             console.log({subscription_created})
-            // await SyncUser.findOneAndUpdate({stripeCusId : subscription_created.customer}, {'$set' : {
-            //     'billing' : {
-            //         prod_id : subscription_created.plan.product,
-            //         subscription_id : subscription_created.id,
-            //         subscription_status : subscription_created.status,
-            //         frequency : subscription_created.plan.interval,
-            //     }
-            // }})
             break;
         case 'customer.subscription.updated':
             const subscription_updated = event.data.object
             console.log({subscription_updated})
             const syncuserDetails = await SyncUser.findOne({stripeCusId : subscription_updated.customer})
             if(!syncuserDetails.billing || syncuserDetails.billing.subscription_id == subscription_updated.id || (syncuserDetails.billing.prod_id != subscription_updated.plan.product && subscription_updated.status == "active")){
-                await SyncUser.findOneAndUpdate({stripeCusId : subscription_updated.customer}, {'$set' : {
-                    'billing' : {
-                        prod_id : subscription_updated.plan.product,
-                        subscription_id : subscription_updated.id,
-                        subscription_status : subscription_updated.status,
-                        frequency : subscription_updated.plan.interval,
-                        amount :  (subscription_updated.plan.amount / 100),
-                        next_billing_date : new Date(subscription_updated.current_period_end * 1000).toLocaleString('en-US')
-                    }
-                }})
+                if(syncuserDetails.billing.prod_id != subscription_updated.plan.product && subscription_updated.status == "active"){
+                    await stripe.subscriptions.cancel(
+                        syncuserDetails.billing.subscription_id
+                      );
+                }
+                const newInfo = await SyncUser.findOneAndUpdate({stripeCusId : subscription_updated.customer}, {'$set' : {
+                        'billing.prod_id' : subscription_updated.plan.product,
+                        'billing.subscription_id' : subscription_updated.id,
+                        'billing.subscription_status' : subscription_updated.status,
+                        'billing.frequency' : subscription_updated.plan.interval,
+                        'billing.amount' :  (subscription_updated.plan.amount / 100),
+                        'billing.next_billing_date' : new Date(subscription_updated.current_period_end * 1000).toLocaleString('en-US')
+                }}, {new : true})
+                console.log({sub_update : newInfo})
             }
             
             break;
@@ -39,8 +35,12 @@ router.post('/stripe/webhook',async (req,res,next)=>{
             break;
         case 'payment_method.attached':
           const paymentMethod = event.data.object;
-          console.log('payeer')
           console.log({paymentMethod})
+          const newInfo = await SyncUser.findOneAndUpdate({stripeCusId : paymentMethod.customer}, {'$set' : {
+                'paymentMethod.last4card_digits' : paymentMethod.card.last4,
+                'paymentMethod.card_brand' : paymentMethod.card.brand
+        }},{new : true})
+        console.log(newInfo)
           // Then define and call a method to handle the successful attachment of a PaymentMethod.
           // handlePaymentMethodAttached(paymentMethod);
           break;
