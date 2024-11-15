a9bd5000f9df1d25358ae19eda011e16db3f9d9d
oladee <deemajor230600@gmail.com>
2024-11-15 16:45:12 +0100

    doney

--- server/utils/kpi.js
+++ server/utils/kpi.js
@@ -1,5 +1,9 @@
 const uploader = require('../models/usermodel').uploader
 const syncUser = require('../models/usermodel').syncUser
+const tracks = require('../models/track.model').track
+const trackLicense = require('../models/track.model').trackLicense
+
+const {crbtRequest,interpolationRequest,fmtRequest,samplingRequest,smcRequest,tvaRequest,videoGamesRequest} = require('../models/quote.model')
 
 const totalUsersKpi = async(queryFilter)=>{
     const currentDate = new Date();
@@ -235,6 +239,126 @@ const inActiveUsersKpi = async(queryFilter)=>{
     }
 }
 
+const totalTracks = async(queryFilter)=>{
+    const currentDate = new Date();
+    let totalTracks = 0
+    let totalTracksKpi = 0
+
+    const currentMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
+    const previousMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - (queryFilter - 1) , 1);
+
+    const countCurrent = await tracks.countDocuments().where('createdAt').lte(currentMonth)
+
+    const countPrevious = await tracks.countDocuments().where('createdAt').lte(previousMonth)
+
+    totalTracks =  countCurrent
+
+    if(queryFilter > 1){
+        totalTracks = countPrevious
+    }
+
+    totalTracksKpi = Math.floor((countCurrent - countPrevious )/ countPrevious * 100)
+    totalTracksKpi = isNaN(totalTracksKpi) ? 0 : totalTracksKpi
+
+    return{
+        totalTracks,
+        totalTracksKpi
+    }
+
+}
+
+const totalLicensedTracks = async(queryFilter)=>{
+    const currentDate = new Date();
+    let totalLicensedTracks = 0
+    let totalLicensedTracksKpi = 0
+    
+    const currentMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
+    const previousMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() - (queryFilter - 1) , 1);
+
+    const countCurrent = await trackLicense.countDocuments().where('createdAt').lte(currentMonth)
+
+    const countPrevious = await trackLicense.countDocuments().where('createdAt').lte(previousMonth)
+
+    totalLicensedTracks =  countCurrent
+    if(queryFilter > 1) {
+        totalLicensedTracks =  countPrevious
+    }
+    totalLicensedTracksKpi = Math.floor((countCurrent - countPrevious )/ countPrevious * 100)
+
+
+    
+    totalLicensedTracksKpi = isNaN(totalLicensedTracksKpi) ? 0 : totalLicensedTracksKpi
+
+    return{
+        totalLicensedTracks,
+        totalLicensedTracksKpi
+    }
+}
+
+const quotesSubmitted = async(queryFilter)=>{
+    const currentDate = new Date();
+    let totalQuotes = 0
+    let totalCurrentQuotes = 0
+    let previousTotalQuotes = 0
+    let quotesSubmittedKpi = 0
+    const currentMonth = new Date(currentDate.getFullYear(), currentDate.getMonth() + 1, 1);
+    const previousMonth = new Date(currentDate.getFullYear(), currentDate.getMonth()- (queryFilter - 1) , 1);
+
+    const [countCurrent1, countCurrent2, countCurrent3, countCurrent4,countCurrent5, countCurrent6, countCurrent7] = await Promise.all([
+        crbtRequest.countDocuments()
+        .where('createdAt').lte(currentMonth),  
+        interpolationRequest.countDocuments()
+        .where('createdAt').lte(currentMonth),
+        fmtRequest.countDocuments()
+        .where('createdAt').lte(currentMonth),  
+        samplingRequest.countDocuments()
+        .where('createdAt').lte(currentMonth),
+        smcRequest.countDocuments()
+        .where('createdAt').lte(currentMonth),  
+        tvaRequest.countDocuments()
+        .where('createdAt').lte(currentMonth),
+        videoGamesRequest.countDocuments()
+        .where('createdAt').lte(currentMonth),  
+        
+    ])
+
+    const [countPrevious1, countPrevious2, countPrevious3, countPrevious4,countPrevious5, countPrevious6, countPrevious7] = await Promise.all([
+        crbtRequest.countDocuments()
+        .where('createdAt').lte(previousMonth),  
+        interpolationRequest.countDocuments()
+        .where('createdAt').lte(previousMonth),
+        fmtRequest.countDocuments()
+        .where('createdAt').lte(previousMonth),  
+        samplingRequest.countDocuments()
+        .where('createdAt').lte(previousMonth),
+        smcRequest.countDocuments()
+        .where('createdAt').lte(previousMonth),  
+        tvaRequest.countDocuments()
+        .where('createdAt').lte(previousMonth),
+        videoGamesRequest.countDocuments()
+        .where('createdAt').lte(previousMonth),  
+        
+    ])
+
+    totalCurrentQuotes = countCurrent1 + countCurrent2 + countCurrent3 + countCurrent4 + countCurrent5 + countCurrent6 + countCurrent7
+
+    previousTotalQuotes = countPrevious1 + countPrevious2 + countPrevious3 + countPrevious4 + countPrevious5 + countPrevious6 + countPrevious7
+
+    totalQuotes = totalCurrentQuotes
+    if(queryFilter > 1){
+        totalQuotes = previousTotalQuotes
+    }
+
+
+    quotesSubmittedKpi = Math.floor((totalCurrentQuotes - previousTotalQuotes )/ previousTotalQuotes * 100)
+    quotesSubmittedKpi = isNaN(quotesSubmittedKpi) ? 0 : quotesSubmittedKpi
+
+    return{
+        totalCurrentQuotes,
+        quotesSubmittedKpi
+    }
+}
+
 
 const metricEvaluator = async(queryFilter)=>{
 
@@ -242,7 +366,11 @@ const metricEvaluator = async(queryFilter)=>{
         ...await totalUsersKpi(queryFilter),
         ...await newUsersKpi(queryFilter),
         ...await activeUsersKpi(queryFilter),
-        ...await inActiveUsersKpi(queryFilter)
+        ...await inActiveUsersKpi(queryFilter),
+        ...await totalTracks(queryFilter),
+        ...await totalLicensedTracks(queryFilter),
+        ...await quotesSubmitted(queryFilter),
+
     }
     return metric
 }
