3e3eab71aacb6e05009d06cc73ae118820334a3c
Oladimeji Momoh <deemajor230600@gmail.com>
2024-12-11 11:39:21 +0000

    slight changes to all tickets returm value

--- server/routes/admin_routes/dispute.js
+++ server/routes/admin_routes/dispute.js
@@ -5,7 +5,7 @@ const asynchandler = require('express-async-handler')
 const { checkAdmin } = require('../../utils/AuthenticateChecker')
 
 router.get('/dispute/all_tickets', checkAdmin, asynchandler(async(req,res,next)=>{
-    const tickets = await ticket.find({}).populate('user').exec()
+    const tickets = await ticket.find({}).populate('user', 'name email userType').populate('associatedDisputes').exec()
     res.send(tickets)
 }))
 
