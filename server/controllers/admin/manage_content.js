620d822389f24c0668f5d0a6f1e75a9808c65893
oladee <deemajor230600@gmail.com>
2025-03-03 12:36:10 +0100

    new rhythm

--- server/controllers/admin/manage_content.js
+++ server/controllers/admin/manage_content.js
@@ -56,18 +56,22 @@ const searchContent = async(req,res,next)=>{
 
 const contentUpdate = async(req,res,next)=>{
     try {
+        console.log(req.body)
         const {_id} = req.body
         if(!mongoose.Types.ObjectId.isValid(_id)){
             throw new BadRequestError("Track not available")
         }
         const trackDetails = await track.findByIdAndUpdate(_id,{...req.body},{new : true}).exec()
+        if(!trackDetails){
+            throw new BadRequestError("Track could not be updated, contact dev team")
+        }
         const activityLog = new adminActivityLog({
             activityDate : Date.now(),
             action_taken : `Updated trackTitle : ${trackDetails.trackTitle}, `,
             performedBy : req.user.id
         })
         await activityLog.save()
-        res.send({message : "TrackDetails uploaded successfully", trackDetails},)
+        res.send({message : "TrackDetails uploaded successfully", trackDetails})
     } catch (error) {
         console.log(error)
         throw new BadRequestError(error.message)
